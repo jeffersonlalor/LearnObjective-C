3932243bc9ea4e8d92378ee5c0c4cd61c263b0b1
jeffoliveira019 <jefferson.lalor@icomp.ufam.edu.br>
2019-07-09 16:13:09 -0400

    Creating Lyrics.h and Lyrics.m files

---
+++ LearnObjective-C/LearnObjective-C/Model/Lyrics.h
@@ -0,0 +1,13 @@
+//
+//  Lyrics.h
+//  LearnObjective-C
+//
+//  Created by Jefferson de Oliveira Lalor on 09/07/19.
+//  Copyright © 2019 Lalor. All rights reserved.
+//
+
+#ifndef Lyrics_h
+#define Lyrics_h
+
+
+#endif /* Lyrics_h */
---
+++ LearnObjective-C/LearnObjective-C/Model/Lyrics.m
@@ -0,0 +1,9 @@
+//
+//  Lyrics.m
+//  LearnObjective-C
+//
+//  Created by Jefferson de Oliveira Lalor on 09/07/19.
+//  Copyright © 2019 Lalor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
